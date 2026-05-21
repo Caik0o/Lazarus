e5539064cb23a8264c49bcb007ef0e0f5a1e6e4b
Caik0_o <vladanaumova18@gmail.com>
2026-05-21 13:19:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,31 @@ const catalogContainer = document.getElementById("catalogContainer");
 const articleContainer = document.getElementById("articleContainer");
 
 function renderAnimals(data) {
+
   if (!catalogContainer) return;
 
   catalogContainer.innerHTML = "";
 
   data.forEach((animal) => {
+
     catalogContainer.innerHTML += `
-    
+
       <a class="animal-card" href="article.html?id=${animal.id}">
-      
+
         <img src="${animal.image}" alt="${animal.name}">
-        
+
         <div class="animal-info">
-        
+
           <h2>${animal.name}</h2>
-          
+
           <h3>${animal.latin}</h3>
-          
+
           <p>${animal.short}</p>
-          
+
         </div>
-        
+
       </a>
-    
+
     `;
   });
 }
@@ -34,7 +36,9 @@ renderAnimals(animals);
 const buttons = document.querySelectorAll(".filter-btn");
 
 buttons.forEach((button) => {
+
   button.addEventListener("click", () => {
+
     buttons.forEach((btn) => btn.classList.remove("active"));
 
     button.classList.add("active");
@@ -42,8 +46,11 @@ buttons.forEach((button) => {
     const category = button.dataset.category;
 
     if (category === "all") {
+
       renderAnimals(animals);
+
     } else {
+
       const filtered = animals.filter(
         (animal) => animal.category === category
       );
@@ -56,7 +63,9 @@ buttons.forEach((button) => {
 const searchInput = document.getElementById("searchInput");
 
 if (searchInput) {
+
   searchInput.addEventListener("input", (e) => {
+
     const value = e.target.value.toLowerCase();
 
     const filtered = animals.filter((animal) =>
@@ -68,6 +77,7 @@ if (searchInput) {
 }
 
 function renderArticle() {
+
   if (!articleContainer) return;
 
   const params = new URLSearchParams(window.location.search);
@@ -77,22 +87,28 @@ function renderArticle() {
   const animal = animals.find((item) => item.id === id);
 
   if (!animal) {
+
     articleContainer.innerHTML = "<h1>Статья не найдена</h1>";
+
     return;
   }
 
   articleContainer.innerHTML = `
-  
-    <img class="article-image" src="${animal.image}">
-    
-    <h1>${animal.name}</h1>
-    
-    <h2>${animal.latin}</h2>
-    
-    <p>
-      ${animal.description}
-    </p>
-  
+
+    <div class="article-content">
+
+      <img class="article-image" src="${animal.image}">
+
+      <h1>${animal.name}</h1>
+
+      <h2>${animal.latin}</h2>
+
+      <p>
+        ${animal.description}
+      </p>
+
+    </div>
+
   `;
 }
 
